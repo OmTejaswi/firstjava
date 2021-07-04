dfdc04200de0107910a786f93c96053fe4127473
OmTejaswi <75362653+OmTejaswi@users.noreply.github.com>
2021-07-04 19:43:46 +0530

    first commit

---
+++ src/README.md
@@ -0,0 +1,8 @@
+<!-- Notes -->
+
+       Data types in java:
+       Two data types in java:
+            1. Primitive -> byte (1 bytes), short (2 bytes), int (4 bytes), long (8 bytes), float (4 bytes)
+                           double (8 bytes), boolean (1 bit), char (2 bytes) 
+
+            2. Non-primitive or refrence -> string (size of object)
---
+++ src/learning.java
@@ -0,0 +1,29 @@
+public class learning {
+    public static void main(String[] args)  {
+        // code starts
+        // System.out.println("Hello, World!");
+
+        // string
+        String name = "harry";
+        System.out.println(name);
+
+        // int ,float,  bool
+        int a = 45;
+        float b = 22.22f;
+        boolean isAdult = true;
+        
+        System.out.println(a + b);
+        System.out.println(isAdult);
+        System.out.println(a);
+
+
+       /* 
+       Data types in java:
+       Two data types in java:
+            1. Primitive -> byte (1 bytes), short (2 bytes), int (4 bytes), long (8 bytes), float (4 bytes)
+                           double (8 bytes), boolean (1 bit), char (2 bytes) 
+
+            2. Non-primitive or refrence -> string (size of object)
+        */
+    }
+}

68e61224a14ba8c289c00dc85da39025665606b1
OmTejaswi <75362653+OmTejaswi@users.noreply.github.com>
2021-07-05 15:43:00 +0530

    reformated

--- src/learning.java
+++ src/learning.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class learning {
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         // code starts
         // System.out.println("Hello, World!");
 
@@ -9,24 +9,22 @@ public class learning {
         String name = "Om Tejaswi";
         System.out.println(name);
 
-        // int ,float,  bool
+        // int ,float, bool
         int a = 45;
         float b = 22.22f;
         boolean isAdult = true;
-        
+
         System.out.println(a + b);
         System.out.println(isAdult);
         System.out.println(a);
 
-
-       /* 
-       Data types in java:
-       Two data types in java:
-            1. Primitive -> byte (1 bytes), short (2 bytes), int (4 bytes), long (8 bytes), float (4 bytes)
-                           double (8 bytes), boolean (1 bit), char (2 bytes) 
-
-            2. Non-primitive or refrence -> string (size of object)
-        */
+        /*
+         * Data types in java: Two data types in java: 1. Primitive -> byte (1 bytes),
+         * short (2 bytes), int (4 bytes), long (8 bytes), float (4 bytes) double (8
+         * bytes), boolean (1 bit), char (2 bytes)
+         * 
+         * 2. Non-primitive or refrence -> string (size of object)
+         */
 
         // Taking user input in java
         Scanner scan = new Scanner(System.in);
@@ -34,9 +32,9 @@ public class learning {
         int age = scan.nextInt();
 
         // If else conditions
-        if(age >= 18) {
+        if (age >= 18) {
             System.out.println("You're adult!");
-        } else if(age <= 18 && age >= 13) {
+        } else if (age <= 18 && age >= 13) {
             System.out.println("You're Teenager");
         } else {
             System.out.println("You're a kid!");
@@ -83,13 +81,13 @@ public class learning {
             case 7:
                 System.out.println("Sunday");
                 break;
-        
+
             default:
                 System.out.println("Sorry, please enter a valid number between 1 - 7");
                 break;
         }
 
-         // loops
+        // loops
         // while loop
         int i = 0;
         while (i < 100) {

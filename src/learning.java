1644ba10bdc03ef07e7be801b3444a7836d66ce4
OmTejaswi <75362653+OmTejaswi@users.noreply.github.com>
2021-07-05 15:39:33 +0530

    week days

--- src/learning.java
+++ src/learning.java
@@ -57,5 +57,36 @@ public class learning {
                 System.out.println("Sorry, the age you have entered did not match in our database.");
                 break;
         }
+
+        // quick quiz:
+        System.out.println("Enter a number between 1 to 7");
+        int day_index = scan.nextInt();
+        switch (day_index) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thrusday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+        
+            default:
+                System.out.println("Sorry, please enter a valid number between 1 - 7");
+                break;
+        }
     }
 }

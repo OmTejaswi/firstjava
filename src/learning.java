b7a8f482cf7dc71111ff135ecbfe500d9609fa3f
OmTejaswi <75362653+OmTejaswi@users.noreply.github.com>
2021-07-05 15:20:13 +0530

    input, if-else, switch case

--- src/learning.java
+++ src/learning.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class learning {
     public static void main(String[] args)  {
         // code starts
@@ -25,5 +27,35 @@ public class learning {
 
             2. Non-primitive or refrence -> string (size of object)
         */
+
+        // Taking user input in java
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter Age: ");
+        int age = scan.nextInt();
+
+        // If else conditions
+        if(age >= 18) {
+            System.out.println("You're adult!");
+        } else if(age <= 18 && age >= 13) {
+            System.out.println("You're Teenager");
+        } else {
+            System.out.println("You're a kid!");
+        }
+
+        // switch statement
+        switch (age) {
+            case 13:
+                System.out.println("You're Teenager");
+                break;
+            case 18:
+                System.out.println("You're teenager");
+            case 56:
+                System.out.println("Oh!....You are old");
+            case 5:
+                System.out.println("What!...You are kid?");
+            default:
+                System.out.println("Sorry, the age you have entered did not match in our database.");
+                break;
+        }
     }
 }
